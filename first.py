aad0e6ad75c883ced9ba86352426a36e6f328caa
Alexander Mamrikishvili <amamalo23@gmail.com>
2024-09-29 17:04:16 +0400

    My First python

---
+++ first.py
@@ -0,0 +1,12 @@
+print("Hello")
+x = 3+5
+print(pow(2415423523652362346234, 77))
+if x > 6 or x < 9 :
+    print("Hello") 
+    print(2)
+elif x > 7 : 
+    print(pow(2415423523652362346234, 77))
+
+for i in range(10) :
+    print(f"i = {i} ") 
+
